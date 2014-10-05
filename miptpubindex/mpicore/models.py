d9f7f6d2bc89fe9cb1968142c7f3d34db66efdfd
Hunternif <hunternif@gmail.com>
2014-10-05 06:20:48 +0400

    Separated Departments and Chairs

--- miptpubindex/mpicore/models.py
+++ miptpubindex/mpicore/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 
 class MiptDepartment(models.Model):
-  name_en = models.CharField(max_length=200)
-  name_ru = models.CharField(max_length=200, blank=True)
+  '''Факультет МФТИ'''
+  name_en = models.CharField(max_length=500, blank=True)
+  name_ru = models.CharField(max_length=500)
   
-  # The metric we assign to our departments:
-  mipt_index = models.IntegerField(default=0)
+  # The metric we assign to our departments & chairs:
+  rank = models.IntegerField(default=0)
   
   def __str__(self):
-    return self.name_en
+    return self.name_ru
+
+
+class MiptChair(models.Model):
+  '''Базовая кафедра МФТИ'''
+  name_en = models.CharField(max_length=500, blank=True)
+  name_ru = models.CharField(max_length=500)
+  department = models.ForeignKey(MiptDepartment)
+  
+  # The metric we assign to our departments & chairs:
+  rank = models.IntegerField(default=0)
+  
+  def __str__(self):
+    return self.name_ru
 
 
 class Author(models.Model):
   author_id = models.CharField(max_length=50)
   name_en = models.CharField(max_length=200)
   name_ru = models.CharField(max_length=200, blank=True)
-  mipt_department = models.ForeignKey(MiptDepartment, blank=True, null=True)
+  mipt_chair = models.ForeignKey(MiptChair, blank=True, null=True)
   h_index = models.IntegerField(default=0)
   institute = models.CharField(max_length=200)
   
@@ -28,6 +42,9 @@ class Journal(models.Model):
   name_en = models.CharField(max_length=200)
   rank_sjr = models.FloatField('SJR (SCImago Journal Rank)', default=0)
   rank_snip = models.FloatField('SNIP (Source Normalized Impact per Paper)', default=0)
+  
+  def __str__(self):
+    return self.name_en
 
 
 class Publication(models.Model):
--- miptpubindex/mpicore/views.py
+++ miptpubindex/mpicore/views.py
@@ -11,17 +11,12 @@ def rows(request):
   data_rows = []
   max_rank = 0
   for dep in MiptDepartment.objects.all():
-    dep_name = ''
-    if len(dep.name_ru) == 0:
-      dep_name = dep.name_en
-    else:
-      dep_name = dep.name_ru
-    data_rows.append([{"name": dep_name, "rank": dep.mipt_index}])
-    if dep.mipt_index > max_rank:
-      max_rank = dep.mipt_index
+    data_rows.append([{"name": dep.name_ru, "rank": dep.rank}])
+    if dep.rank > max_rank:
+      max_rank = dep.rank
   # Update percentages:
   if max_rank > 0:
     for data_row in data_rows:
-      data_row[0]['percent'] = data_row[0]['rank'] / max_rank * 100
+      data_row[0]['percent'] = 100. * data_row[0]['rank'] / max_rank
   
   return render(request, 'index.html', {"rows": data_rows, "max_rank": max_rank})

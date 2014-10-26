2342ffb5482c201647ca3fdac8ab7bfd046816bc
DanSkeel <DanSkeel@gmail.com>
2014-10-26 23:09:45 +0300

    Found useful api that allows us to define filters for admin pages. Added two filters to find Publications without Affiliations and without Authors. Also enabled search field for Publications and Affiliations

--- miptpubindex/mpicore/admin.py
+++ miptpubindex/mpicore/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from mpicore.models import *
+from django.utils.translation import ugettext_lazy as _
+from django.db.models.aggregates import Count
 
+class PublicationSpecialCases(admin.SimpleListFilter):
+    title = _('Special cases')
+    parameter_name = 'SpecialCase'
+    def lookups(self, request, model_admin):
+        return (
+            ('NoAff', _('No affiliations')),
+            ('NoAuthors', _('No authors')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'NoAff':
+            return queryset.annotate(aff_count=Count('affiliation')).filter(aff_count=0)
+        if self.value() == 'NoAuthors':
+            return queryset.annotate(auth_count=Count('author')).filter(auth_count=0)
+        
 class AuthorInline(admin.TabularInline):
   model = Publication.author.through
   extra = 3
@@ -9,16 +26,20 @@ class AffiliationInline(admin.TabularInline):
   model = Publication.affiliation.through
   extra = 2
 
+class AffiliationAdmin(admin.ModelAdmin):
+    search_fields = ['af_id','name_en']
+    
 class PublicationAdmin(admin.ModelAdmin):
   inlines = (
     AuthorInline,
     AffiliationInline
   )
   fields = ('date', 'name_en', 'name_ru', 'eid', 'doi', 'journal', 'citations')
-
+  search_fields = ['eid','name_en']
+  list_filter = (PublicationSpecialCases,)
 
 admin.site.register(Journal)
-admin.site.register(Affiliation)
+admin.site.register(Affiliation, AffiliationAdmin)
 admin.site.register(Publication, PublicationAdmin)
 admin.site.register(Author)
 admin.site.register(MiptDepartment)
